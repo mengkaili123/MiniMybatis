005ca44deb82771c9dd83eaa18ac937b74352400
孟铠立 <mengkaili@mengkailideMacBook-Air.local>
2021-10-08 00:32:57 +0800

    first commit

---
+++ src/main/java/com/mkl/mybaitis/session/SqlSession.java
@@ -0,0 +1,4 @@
+package com.mkl.mybaitis.session;
+
+public interface SqlSession {
+}
---
+++ src/main/java/com/mkl/mybaitis/session/SqlSessionFactoryBuilder.java
@@ -0,0 +1,7 @@
+package com.mkl.mybaitis.session;
+
+/**
+ * 读取配置文件信息，进行构造SqlSessionFacTory
+ */
+public class SqlSessionFactoryBuilder {
+}
